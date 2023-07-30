0391f5035d75b6f8badfbcd530bb79f30bca8d94
MK <mk1995@users.noreply.github.com>
2023-07-30 23:04:29 +0545

    Update file_read.c

--- File Handling/file_read.c
+++ File Handling/file_read.c
@@ -32,6 +32,20 @@ int main()
         int a;
         char n[50];
         /*
+        %49[^\t\n]: This format specifier is used to read a string from the file until it encounters either a tab (\t) character or a newline (\n) character, or it reaches the maximum length of 49 characters (whichever comes first). Let's break it down further:
+
+        49: The number 49 is a width specifier that limits the number of characters read by %[^\t\n] to 49 (excluding the null-terminator). This ensures that the read string doesn't exceed the size of the character array n (which is 50 characters).
+
+        [^\t\n]: The [...] is a scanset or character set in fscanf. It allows you to specify a set of characters to be accepted as input. In this case, ^\t\n means any character except for tab or newline. So, %[^\t\n] will read characters until it encounters a tab or newline character.
+
+        \t\t\t\t\t: This part of the format specifier matches exactly five tab characters. This is used to consume the tabs following the name string, ensuring the fscanf function expects to find five tabs after reading the name.
+
+        %d: This format specifier is used to read an integer from the file.
+
+        \n: This part of the format specifier matches a newline character, ensuring the fscanf function expects to find a newline character after reading the integer.
+        */
+        
+        /*
         the format specifier %49[^\t\n]\t\t\t\t\t%d\n is used to read a name 
         (a sequence of characters that doesn't contain tabs or newlines) followed by exactly five tabs and an integer, ending with a newline character.
         The name is stored in the character array n, and the integer is stored in the variable a. 
